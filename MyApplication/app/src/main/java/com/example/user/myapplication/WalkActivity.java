76d3a6973917f845f9aba748973bedb667d57b23
JimmyCheng059 <cyc059@ucsd.edu>
2016-02-24 16:22:05 -0800

    SeekBar

--- MyApplication/app/src/main/java/com/example/user/myapplication/WalkActivity.java
+++ MyApplication/app/src/main/java/com/example/user/myapplication/WalkActivity.java
@@ -3,19 +3,47 @@ package com.example.user.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
+
 
 public class WalkActivity extends AppCompatActivity {
 
-    TextView bus_num_ucsd = null;
+    private TextView speedText;
+    private SeekBar speed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.walk_ask);
 
-        Global g = (Global)getApplication();
+        Global g = (Global) getApplication();
+        speed = (SeekBar) findViewById(R.id.seekBar);
+        speedText = (TextView) findViewById(R.id.speedText);
+
+        speedText.setText(speed.getProgress() + "/" + speed.getMax());
+        speed.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Do something here,
+                //if you want to do anything at the start of
+                // touching the seekbar
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Display the value in textview
+                speedText.setText(progress + "/" + seekBar.getMax());
+            }
+        });
 
         if(g.getData_bus_kind()=="MTS"){
             String text= "MTS";
@@ -31,28 +59,5 @@ public class WalkActivity extends AppCompatActivity {
             ImageView img= (ImageView) findViewById(R.id.location_choice);
             img.setImageResource(R.drawable.ucsd);
         }
-
-        if(g.getData_method()=="bus_stop"){
-            Bundle bundle = getIntent().getExtras();
-            String text= bundle.getString("UCSD_BUS_NUM");
-
-            LinearLayout lView = (LinearLayout)findViewById(R.id.map_layout);
-
-            bus_num_ucsd = new TextView(this);
-            bus_num_ucsd.setText(text);
-
-            lView.addView(bus_num_ucsd);
-        }
-        else{
-            String text= "123";
-            LinearLayout lView = (LinearLayout)findViewById(R.id.map_layout);
-
-            bus_num_ucsd = new TextView(this);
-            bus_num_ucsd.setText(text);
-
-            lView.addView(bus_num_ucsd);
-        }
     }
-
-
 }
